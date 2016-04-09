0c0b0c9f5b9bd186a02cce3c3bf10691584787ef
valGarcia <valerie.garcia01@gmail.com>
2016-04-09 09:40:47 -0700

    Highlight
    Figured out how to highlight words

--- js/content.js
+++ js/content.js
@@ -4,7 +4,7 @@ function getText(){
 function getHTML(){
     return document.body.outerHTML
 }
-console.log(getText());             //Gives you all the text on the page
+//console.log(getText());             //Gives you all the text on the page
 //console.log(getHTML());             //Gives you the whole HTML of the page
 
 // chrome.extension.onRequest.addListener(
@@ -14,3 +14,17 @@ console.log(getText());             //Gives you all the text on the page
 //         }
 //     }
 // );
+
+// $html = $("body").find("article").;
+// console.log($("body").find("article").html());
+getArticle();
+
+function getArticle(){
+    var art = document.getElementsByClassName("story-body-text story-content");
+    //art[0].innerHTML.css("color", "blue");
+    for(var i = 0, length = art.length; i < length; i++){
+        var x = art[i].innerHTML.replace("A few months", "<mark>A few months</mark>");
+        art[i].innerHTML = x;
+    }
+    console.log(art);
+}
