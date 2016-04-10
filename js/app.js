02e16200dc922f7580898bc6c29f7c0e80b93ce2
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2016-04-09 21:11:25 -0700

    last changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
   $("#read_now").click(function(){
     console.log("clicked!");
-    
-
     chrome.tabs.getCurrent(function(tab) {
 	 chrome.tabs.executeScript({
 	 	code: "replaceText()"
 	 });
 	});
 
+  });
+
+  $("#start_button").click(function(event){
+    console.log("clicked!");
+    startButton(event);
   });
 });
 
